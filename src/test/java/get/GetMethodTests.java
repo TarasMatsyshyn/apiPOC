f249a6590b3c75262483933d0f4066737faec17f
Taras Matsyshyn <Taras_Matsyshyn@epam.com>
2025-02-13 13:30:17 +0200

    Add test logging step

--- src/test/java/get/GetMethodTests.java
+++ src/test/java/get/GetMethodTests.java
@@ -18,7 +18,7 @@ import static org.hamcrest.Matchers.notNullValue;
 public class GetMethodTests extends BaseTest {
 
     @Test
-    public void testThatUserContainsEmailTest(){
+    public void testThatUserContainsEmailTest() {
         List<UserData> users = given()
                 .when()
                 .contentType(ContentType.JSON)
@@ -26,14 +26,20 @@ public class GetMethodTests extends BaseTest {
                 .then().log().all()
                 .extract().body().jsonPath().getList("data", UserData.class);
 
-        users.forEach(x-> Assertions.assertTrue(x.getAvatar().contains(x.getId().toString())));
+        // Логування результатів
+        users.forEach(x -> Assertions.assertTrue(x.getAvatar().contains(x.getId().toString())));
         Assertions.assertTrue(users.stream().allMatch(x -> x.getEmail().endsWith("@reqres.in")));
 
         List<String> avatars = users.stream().map(UserData::getAvatar).collect(Collectors.toList());
-        List<String> ids = users.stream().map(x->x.getId().toString()).collect(Collectors.toList());
+        List<String> ids = users.stream().map(x -> x.getId().toString()).collect(Collectors.toList());
         for (int i = 0; i < avatars.size(); i++) {
             Assertions.assertTrue(avatars.get(i).contains(ids.get(i)));
         }
+
+        // Логування додаткової інформації для Jenkins
+        System.out.println("User List: " + users);
+        System.out.println("Avatars: " + avatars);
+        System.out.println("IDs: " + ids);
     }
 
     @Test
